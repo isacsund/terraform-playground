c17f71be0c3debb6c9971c84561d139265717b99
Isac Sund <isac.sund@outlook.com>
2020-08-01 22:02:15 +0300

    Copy over public ssh key to VMs

--- gitlab-runners-k8s/main.tf
+++ gitlab-runners-k8s/main.tf
@@ -46,3 +46,27 @@ resource "lxd_container" "node-2" {
     memory = "1024MB"
   }
 }
+
+resource "lxd_container_file" "master_ssh_keys" {
+  container_name     = lxd_container.k8s-master.name
+  target_file        = "/root/.ssh/authorized_keys"
+  source             = "/home/isac/.ssh/id_rsa.pub"
+  mode               = "0640"
+  create_directories = true
+}
+
+resource "lxd_container_file" "node-1_ssh_keys" {
+  container_name     = lxd_container.node-1.name
+  target_file        = "/root/.ssh/authorized_keys"
+  source             = "/home/isac/.ssh/id_rsa.pub"
+  mode               = "0640"
+  create_directories = true
+}
+
+resource "lxd_container_file" "node-2_ssh_keys" {
+  container_name     = lxd_container.node-2.name
+  target_file        = "/root/.ssh/authorized_keys"
+  source             = "/home/isac/.ssh/id_rsa.pub"
+  mode               = "0640"
+  create_directories = true
+}
